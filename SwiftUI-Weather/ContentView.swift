0e48f72a4444cea6ed50601c461f3b6bc9fac893
Naoki Takahashi <naokitakabusiness@gmail.com>
2023-10-12 13:16:55 +0900

    Initial Commit

---
+++ SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by Naoki Takahashi on 10/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
