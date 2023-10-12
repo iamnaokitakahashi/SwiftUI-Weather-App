01d18af53df557985fcf569232f5616855e70332
Naoki Takahashi <naokitakabusiness@gmail.com>
2023-10-12 15:52:18 +0900

    refactored BackgroundView, CityTextView, MainWeatherStatusView, and WeatherButton view

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -10,30 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, Color("lightBlue")]),
-                           startPoint: .topLeading,
-                           endPoint: .bottomTrailing)
-            .edgesIgnoringSafeArea(.all)
+            BackgroundView(topColor: .blue, bottomColor: Color("lightBlue"))
             
             VStack {
-                Text("Marin, CA")
-                    .font(.system(size: 32, weight: .medium, design: .default))
-                    .foregroundColor(.white)
-                    .padding() // you can put like 100, .buttom its very flexible
+                CityTextView(cityName: "Marin, CA")
                 
-                VStack(spacing: 10) { // can play around with spacing between
-                    Image(systemName: "cloud.sun.fill") // systemNamefor SF symbols
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit) // doesn;t look strethced anymore
-                        .frame(width: 180, height: 180) // keep this frame square because SF S has them squared
-                    
-                    Text("76°") // option + shift + 8 = °
-                        .font(.system(size: 70, weight: .medium))
-                        .foregroundColor(.white)
-                    
-                }
-                .padding(.bottom, 40) // can use instead of spacing
+                MainWeatherStatusView(imageName: "cloud.sun.fill", temperature: 76)
                 
                 HStack(spacing: 20) {
                     WeatherDayView(dayOfWeek: "TUE",
@@ -62,11 +44,7 @@ struct ContentView: View {
                 Button {
                     print("tapped")
                 } label: {
-                    Text("Change Day Time")
-                        .frame(width: 280, height: 50)
-                        .background(Color.white)
-                        .font(.system(size: 20, weight: .bold, design: .default))
-                        .cornerRadius(10) //10 is apple looking bottom
+                    WeatherButton(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
                 }
                 
                 Spacer() // don't overuse spacers
@@ -104,4 +82,69 @@ struct WeatherDayView: View { // everyting in swift is VIEW
                 .foregroundColor(.white)
         }
     }
+}
+
+struct BackgroundView: View {
+    
+    var topColor: Color
+    var bottomColor: Color
+    
+    var body: some View {
+        
+        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CityTextView: View {
+    var cityName: String
+    var body: some View {
+        VStack {
+            Text(cityName)
+                .font(.system(size: 32, weight: .medium, design: .default))
+                .foregroundColor(.white)
+                .padding() // you can put like 100, .buttom its very flexible
+        }
+    }
+}
+
+struct MainWeatherStatusView: View {
+    
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing: 10) { // can play around with spacing between
+            Image(systemName: imageName) // systemNamefor SF symbols
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit) // doesn;t look strethced anymore
+                .frame(width: 180, height: 180) // keep this frame square because SF S has them squared
+            
+            Text("\(temperature)") // option + shift + 8 = °
+                .font(.system(size: 70, weight: .medium))
+                .foregroundColor(.white)
+            
+        }
+        .padding(.bottom, 40) // can use instead of spacing
+    }
+}
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10) //10 is apple looking bottom
+    }
+    
 }
